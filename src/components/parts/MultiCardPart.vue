c49f4e5f99ed40e1de39d2c7b111658d01ae7e03
Daniel2193 <daniel2193mc@gmail.com>
2025-10-06 15:13:02 +0200

    NEW: MultiPart Url New Tab

--- src/components/parts/MultiCardPart.vue
+++ src/components/parts/MultiCardPart.vue
@@ -1,8 +1,8 @@
 <template>
 	<div v-if="arr" :class="class">
 		<template v-for="item in arr">
-			<Button v-if="item.url" variant="link" @click="() => openUrl(item.url!)" :class="item.classes" class="p-0 cursor-pointer h-max">{{ item.text }}</Button>
-			<span v-else :class="item.classes" class="h-max">{{ item.text }}</span>
+			<Button v-if="item.url" variant="link" @click="() => openUrl(item.url!, item.sameTab)" :class="(item.classes + ' ' + item.className).trim()" class="p-0 cursor-pointer h-max">{{ item.text }}</Button>
+			<span v-else :class="((item.classes ?? '') + ' ' + (item.className ?? '')).trim()" class="h-max">{{ item.text }}</span>
 			<br v-if="item.newLine" />
 		</template>
 	</div>
@@ -12,8 +12,12 @@
 import { MultiTextCardProps } from "@/lib/common.types";
 import Button from "../ui/button/Button.vue";
 
-function openUrl(url: string) {
-	window.location.replace(url);
+function openUrl(url: string, sameTab = false) {
+    if(sameTab){
+        window.location.replace(url)
+    }else{
+        window.open(url);
+    }
 }
 
 defineProps<{ arr?: MultiTextCardProps[]; class?: string }>();
