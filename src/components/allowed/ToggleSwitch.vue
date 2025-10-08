93ff3b074254c0e7087a2e3ff180d438b789965a
Daniel2193 <daniel2193mc@gmail.com>
2025-10-08 13:11:17 +0200

    FIX: ToggleSwitch

--- src/components/allowed/ToggleSwitch.vue
+++ src/components/allowed/ToggleSwitch.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue';
 import Switch from '../ui/switch/Switch.vue';
 const props = defineProps<{initialValue?: boolean, label?: string}>()
-const state = ref(false)
+const state = ref(!!props.initialValue)
 const emit = defineEmits<(e: 'update', state: boolean) => void>()
 </script>
 
